8a4577b7e535af9c8305085226098e7c15bf1aee
sharkdp <davidpeter@web.de>
2015-03-08 14:17:55 +0100

    Migrate from loadash to rambda, leading to MUCH cleaner code.

--- js/main.js
+++ js/main.js
@@ -1,75 +1,55 @@
 /*jslint browser: true, nomen: true, white: true, vars: true*/
-/*global _, Isomer*/
+/*global R, Isomer*/
+
+'use strict';
 
 (function () {
-    "use strict";
-
-    // flatMap (aka concatMap)
-    _.mixin({
-        flatMap: _.compose(_.flatten, _.map)
-    });
-
     var Shape = Isomer.Shape;
     var Point = Isomer.Point;
     var Color = Isomer.Color;
-    var Path = Isomer.Path;
 
-    // http://www.colourlovers.com/palette/148712/Gamebookers
-    // var colorLine = [
-    //     new Color(255, 153, 0),
-    //     new Color(66, 66, 66),
-    //     new Color(233, 233, 233),
-    //     new Color(188, 188, 188),
-    //     new Color(50, 153, 187)
-    // ];
+    // Global isomer instance
+    var isomer;
 
-    // http://www.colourlovers.com/palette/1473/Ocean_Five
-    var colorLine = [
-        new Color(0, 160, 176),
-        new Color(106, 74, 60),
-        new Color(204, 51, 63),
-        new Color(235, 104, 65),
-        new Color(237, 201, 81)
-    ];
-
-    var Game = function() {
-        this.canvas = document.getElementById("canvas");
-        this.iso = new Isomer(this.canvas);
-        this.iso.scale = 54;
-        this.xmax = 4;
-        this.ymax = 13;
-
-        _.bindAll(this);
+    var config = {
+        // http://www.colourlovers.com/palette/1473/Ocean_Five
+        colors: [
+            new Color(0, 160, 176),
+            new Color(106, 74, 60),
+            new Color(204, 51, 63),
+            new Color(235, 104, 65),
+            new Color(237, 201, 81)
+        ],
+        ymax: 13,
+        xmax: 4
     };
 
-    Game.prototype.renderBlock = function(x, y, value, z) {
-        this.iso.add(
+    var renderBlock = function(x, y, value, z) {
+        isomer.add(
             new Shape.Prism(
-                new Point(4.2 * (this.xmax - x), this.ymax - y, z),
+                new Point(4.2 * (config.xmax - x), config.ymax - y, z),
                 1, 1, 1
             ),
-            colorLine[value]
+            config.colors[value]
         );
     };
 
-    Game.prototype.renderBlocks = function(x, value, y) {
-        if (_.isArray(value)) {
-            _.each(value, _.partial(this.renderBlock, x, y));
-        } else {
-            this.renderBlock(x, y, value, 0);
-        }
+    var renderBlocks = function(x, value, y) {
+        R.forEachIndexed(R.partial(renderBlock, x, y), value);
     };
 
-    Game.prototype.renderLine = function(line, x) {
-        _.each(line, _.partial(this.renderBlocks, x));
+    var renderLine = function(line, x) {
+        R.forEachIndexed(R.partial(renderBlocks, x), line);
     };
 
-    Game.prototype.renderLines = function(lines) {
-        _.each(lines.reverse(), this.renderLine);
+    var renderLines = function(lines) {
+        R.forEachIndexed(renderLine, lines.reverse());
     };
 
     window.onload = function() {
-        var game = new Game();
+        var canvas = document.getElementById("canvas");
+        isomer = new Isomer(canvas);
+        isomer.scale = 40;
 
         var BL = 0;
         var BR = 1;
@@ -77,116 +57,40 @@
         var OR = 3;
         var YE = 4;
 
-        var equal = _.curry(function(x, y) { return x === y; });
-        var notEqual = _.curry(function(x, y) { return x !== y; });
-
-        var a2b = _.curry(function(a, b, x) {
+        var replace = R.curry(function(a, b, x) {
             return (x === a) ? b : x;
         });
-        var a2bc = _.curry(function(a, b, c, x) {
-            return (x === a) ? [b, c] : x;
-        });
 
-        var clone = function(x) { return [x, x]; };
+        // TODO: with the new rambda > 0.10, this can be replace by: R.repeat(R.__, 2);
+        var clone = R.partialRight(R.repeat, 2);
 
-        var _map = _.curryRight(_.map, 2);
-        var _flatmap = _.curryRight(_.flatMap, 2);
-        var _filter = _.curryRight(_.filter, 2);
+        var map2d = function(f) { return R.map(R.map(f)); };
+        var filter2d = function(f) { return R.map(R.filter(f)); };
+        var reject2d = function(f) { return R.map(R.reject(f)); };
+        var wrap = R.map(R.of);
+        var flatten = R.compose(wrap, R.flatten);
+        // var flatmap = R.compose(flatten, map2d);
 
-        var _deepmap = _.curry(function(f, xs) {
-            return _.map(xs, function(x) {
-                if (_.isArray(x)) {
-                    return _.map(x, f);
-                }
-                return f(x);
-            });
-        });
-        var _deepflatmap = _.curry(function(f, xs) {
-            return _.flatten(_deepmap(f)(xs));
-        });
-        var _deepfilter = _.curry(function(f, xs) {
-            return _.reduce(xs, function(ys, x) {
-                if (_.isArray(x)) {
-                    var xf = _.filter(x, f);
-                    if (xf.length > 0) {
-                        ys.push(xf);
-                    }
-                } else {
-                    if (f(x)) {
-                        ys.push(x);
-                    }
-                }
-                return ys;
-            }, []);
-        });
-
-        var cleanup = _map(function(x) {
-            if (_.isArray(x)) {
-                if (x.length === 1) {
-                    return x[0];
-                }
-                return _.flattenDeep(x);
-            }
-            return x;
-        });
-
-        var randInt = function(range) {
-            return Math.floor(range * Math.random());
-        };
-
-        var randomColor = function() {
-            return _.sample([BL, RE]);
-        };
-
-        var randomArgs = function(f, nArgs) {
-            var arg = randomColor();
-            if (nArgs === 1) {
-                return f(arg);
-            }
-            return randomArgs(f(arg), nArgs - 1);
-        };
-
-        var randomF = function() {
-            var i1, f1, f2;
-            i1 = randInt(10);
-            if (i1 < 8) { // map or flatmap
-                f1 = _.sample([_deepmap, _deepflatmap]);
-                f2 = _.sample([
-                    randomArgs(a2b, 2),
-                    randomArgs(a2bc, 3)
-                ]);
-                return f1(f2);
-            } // filter
-            f2 = _.sample([equal, notEqual]);
-            return _deepfilter(randomArgs(f2, 1));
-        };
 
         var queue = [
-            _flatmap(clone),
-            // _deepflatmap(a2bc(BR, BR, YE)),
-            _deepmap(a2bc(BR, BR, YE)),
-            _deepmap(a2bc(BR, BR, YE)),
-            _deepfilter(equal(YE))
-            // _flatmap(a2bc(YE, YE, BR)),
-            // _map(a2bc(BR, BR, YE)),
-            // _flatmap(blueToBD),
-            // _filter(notEqual(1)),
-            // _map(clone),
+            map2d(clone),
+            flatten,
+            map2d(replace(YE, [YE, BR])),
+            map2d(replace(BR, [RE, BR])),
+            R.reject(R.eqDeep([RE, BR]))
+            // reject2d(R.eq(BR)),
         ];
 
-        var initial = [BR, YE, BR];
+        var initial = wrap([YE, BR, YE]);
 
-        // queue = [ randomF(), randomF(), randomF(), randomF(), randomF() ];
-        // initial = [ randomColor(), randomColor(), randomColor() ];
+        var clearEmpty = R.filter(R.not(R.eqDeep([])));
+        var cleanup = R.compose(clearEmpty, R.map(R.flatten));
 
-        var lines = _.reduce(queue, function(ls, func) {
-            var newLine = cleanup(func(_.last(ls)));
-            ls.push(newLine);
-            return ls;
-        }, [initial]);
+        var lines = R.scan(function(ls, func) {
+            return cleanup(func(ls));
+        }, initial, queue);
 
-        _.each(lines, function(l) {console.log(l);});
-
-        game.renderLines(lines);
+        R.forEach(function(l) {console.log(l);}, lines);
+        renderLines(lines);
     };
 }());
