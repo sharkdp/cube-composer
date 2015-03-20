f8d8a5a3adc1bbdfeab892dffc94a8d04bcd6783
sharkdp <davidpeter@web.de>
2015-03-20 17:32:00 +0100

    Enum instance

--- src/CubeComposer.purs
+++ src/CubeComposer.purs
@@ -1,22 +1,51 @@
 module CubeComposer where
 
 import Data.Array
+import Data.Enum
 import Data.Foldable
 import Data.Maybe
 import Debug.Trace
 
-data Cube = Red | Blue
+data Cube = Blue | Brown | Red | Orange | Yellow
 
 instance showCube :: Show Cube where
-    show Red = "Red"
     show Blue = "Blue"
+    show Brown = "Brown"
+    show Red = "Red"
+    show Orange = "Orange"
+    show Yellow = "Yellow"
 
 instance eqCube :: Eq Cube where
-    (==) Red Red = true
-    (==) Blue Blue = true
-    (==) Red Blue = false
-    (==) Blue Red = false
-    (/=) x y = not (x == y)
+    (==) a b = fromEnum a == fromEnum b
+    (/=) a b = not (a == b)
+
+instance ordCube :: Ord Cube where
+    compare a b = fromEnum a `compare` fromEnum b
+
+instance enumCube :: Enum Cube where
+    cardinality = Cardinality 5
+    firstEnum = cubeFirst
+    lastEnum = cubeLast
+    succ = defaultSucc cubeToEnum cubeFromEnum
+    pred = defaultPred cubeToEnum cubeFromEnum
+    toEnum = cubeToEnum
+    fromEnum = cubeFromEnum
+
+cubeFirst = Blue
+cubeLast = Yellow
+
+cubeFromEnum Blue = 0
+cubeFromEnum Brown = 1
+cubeFromEnum Red = 2
+cubeFromEnum Orange = 3
+cubeFromEnum Yellow = 4
+
+cubeToEnum 0 = Just Blue
+cubeToEnum 1 = Just Brown
+cubeToEnum 2 = Just Red
+cubeToEnum 3 = Just Orange
+cubeToEnum 4 = Just Yellow
+cubeToEnum _ = Nothing
 
 type Stack = [Cube]
 type Wall = [Stack]
