a55e943a438e8f8178eeb9b4cf7a1e424ba8aea6
sharkdp <davidpeter@web.de>
2015-05-29 21:16:43 +0200

    Most run-time errors will be caused by a messed-up global state. Chance to recover by clearing localStorage.

--- src/Unsafe.purs
+++ src/Unsafe.purs
@@ -4,6 +4,13 @@ module Unsafe where
 foreign import unsafeError
     """
     function unsafeError(msg) {
+        // Try to recover from this error by resetting the global state.
+        if (confirm(msg + ". Clear localStorage and reload?")) {
+           localStorage.clear();
+           location.reload();
+        };
+
+        // Abort execution
         throw new Error(msg);
     }
     """ :: forall a. String -> a
