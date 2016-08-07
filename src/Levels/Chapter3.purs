0aa44338205e7c8a4ed83efae5d5e99562aecffa
sharkdp <davidpeter@web.de>
2016-08-07 16:51:57 +0200

    Switch chapter 3 and 4

--- src/Levels/Chapter3.purs
+++ src/Levels/Chapter3.purs
@@ -1,96 +1,69 @@
 module Levels.Chapter3 where
 
 import Prelude
-import Data.Foldable (sum)
-import Data.Int (even)
-import Data.Int.Bits ((.&.))
-import Data.List (List(..), filter, fromFoldable, (:), zipWith)
+import Data.List (List(..), (:), concatMap)
 import Data.Maybe (Maybe(..))
 
-import Helper (AStack, fromArray, (:->), (:>))
-import Types (Chapter, Transformer, Stack, Cube(..), Difficulty(..))
+import Helper (fromArray, (:->), (:>))
+import Transformer (replaceSingle)
+import Types (Chapter, Stack, Cube(..), Difficulty(..))
 
-toDigit :: Cube -> Int
-toDigit Orange = 0
-toDigit _      = 1
+cxToX :: Stack -> Stack
+cxToX Nil                     = Nil
+cxToX (Cons Cyan (Cons y xs)) = y : cxToX xs
+cxToX (Cons x xs)             = x : cxToX xs
 
-toCube :: Int -> Cube
-toCube 0 = Orange
-toCube _ = Brown
-
-toInt :: Stack -> Int
-toInt w = sum $ zipWith (\f c -> f * toDigit c) (1 : 2 : 4 : Nil) w
-
--- | Get the (first three bits of the) binary representation of a number
-digits :: Int -> Array Int
-digits n = map bit [1, 2, 4]
-    where bit m = if n .&. m == m then 1 else 0
-
-toAStack :: Int -> AStack
-toAStack num = map toCube (digits num)
-
-toStack :: Int -> Stack
-toStack = fromFoldable <<< toAStack
-
-mapNumbers :: (Int -> Int) -> Transformer
-mapNumbers f = map (toInt >>> f >>> toStack)
+ooToC :: Stack -> Stack
+ooToC Nil                            = Nil
+ooToC (Cons Orange (Cons Orange xs)) = Cyan : ooToC xs
+ooToC (Cons x cs)                    = x : ooToC cs
 
 chapter3 :: Chapter
 chapter3 = {
     name: "Chapter 3",
 
     transformers: fromArray [
-        "mapAdd1" :> {
-            name: "map (+1)",
-            function: mapNumbers (_ + 1)
+        "mapXtoOX" :> {
+            name: "map {X}↦[{X}{Orange}]",
+            function: map (concatMap (\x -> (Orange : x : Nil)))
         },
-        "mapSub1" :> {
-            name: "map (-1)",
-            function: mapNumbers (_ - 1)
+        "mapCXtoX" :> {
+            name: "map [{X}{Cyan}]↦{X}",
+            function: map cxToX
         },
-        "mapMul2" :> {
-            name: "map (×2)",
-            function: mapNumbers (_ * 2)
+        "mapOOtoC" :> {
+            name: "map [{Orange}{Orange}]↦{Cyan}",
+            function: map ooToC
         },
-        "mapPow2" :> {
-            name: "map (^2)",
-            function: mapNumbers (\x -> x * x)
-        },
-        "filterEven" :> {
-            name: "filter even",
-            function: filter (toInt >>> even)
+        "mapCtoO" :> {
+            name: "map {Cyan}↦{Orange}",
+            function: replaceSingle Cyan Orange
         }
     ],
 
     levels: fromArray [
         "3.1" :-> {
-            name: "0b0 .. 0b111",
-            help: Just """What could be the meaning of the title <code>0b0 .. 0b111</code>?
-                          Read from top to bottom. Calculate modulo eight.""",
-            difficulty: Medium,
-            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
-            target: map toAStack [1, 3, 5, 7, 1, 3, 5, 7]
+            name: "Brick",
+            help: Just """This chapter introduces wildcard cubes: {X}.""",
+            difficulty: Easy,
+            initial: [[Cyan, Orange], [Cyan, Cyan, Orange], [Orange, Orange], [Cyan, Cyan, Orange], [Cyan, Orange]],
+            target: [[Cyan], [Cyan, Orange], [Cyan], [Cyan, Orange], [Cyan]]
         },
         "3.2" :-> {
-            name: "Odd..",
+            name: "Fort",
             help: Nothing,
-            difficulty: Easy,
-            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
-            target: map toAStack [1, 3, 5, 7]
+            difficulty: Hard,
+            initial: [[Cyan, Orange], [Cyan, Cyan, Orange], [Orange, Orange], [Cyan, Cyan, Orange], [Cyan, Orange]],
+            target: [[Orange, Cyan], [Orange, Orange], [Orange, Cyan], [Orange, Orange], [Orange, Cyan]]
         },
         "3.3" :-> {
-            name: "Zero",
-            help: Nothing,
-            difficulty: Hard,
-            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
-            target: map toAStack [0, 0, 0, 0, 0, 0, 0, 0]
-        },
-        "3.4" :-> {
-            name: "Don't panic",
-            help: Just """Life, the Universe and Everything...""",
-            difficulty: Hard,
-            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
-            target: map toAStack [4, 2, 4, 2, 4, 2, 4, 2]
+            name: "Castle",
+            help: Just """This is the last level ... for now. But you can design your own puzzles!
+                          See the <a href="https://github.com/sharkdp/cube-composer">Github repository</a>
+                                           for more information. I hope you enjoyed the game.""",
+            difficulty: Medium,
+            initial: [[Orange], [Orange, Orange], [Orange, Orange, Orange], [Orange, Orange, Orange, Orange], [Orange, Orange, Orange], [Orange, Orange], [Orange]],
+            target: [[Orange, Orange], [Orange, Cyan], [Orange, Orange], [Orange, Cyan], [Orange, Orange], [Orange, Cyan], [Orange, Orange]]
         }
     ]
 }
--- src/Levels/Chapter4.purs
+++ src/Levels/Chapter4.purs
@@ -1,69 +1,96 @@
 module Levels.Chapter4 where
 
 import Prelude
-import Data.List (List(..), (:), concatMap)
+import Data.Foldable (sum)
+import Data.Int (even)
+import Data.Int.Bits ((.&.))
+import Data.List (List(..), filter, fromFoldable, (:), zipWith)
 import Data.Maybe (Maybe(..))
 
-import Helper (fromArray, (:->), (:>))
-import Transformer (replaceSingle)
-import Types (Chapter, Stack, Cube(..), Difficulty(..))
+import Helper (AStack, fromArray, (:->), (:>))
+import Types (Chapter, Transformer, Stack, Cube(..), Difficulty(..))
 
-cxToX :: Stack -> Stack
-cxToX Nil                     = Nil
-cxToX (Cons Cyan (Cons y xs)) = y : cxToX xs
-cxToX (Cons x xs)             = x : cxToX xs
+toDigit :: Cube -> Int
+toDigit Orange = 0
+toDigit _      = 1
 
-ooToC :: Stack -> Stack
-ooToC Nil                            = Nil
-ooToC (Cons Orange (Cons Orange xs)) = Cyan : ooToC xs
-ooToC (Cons x cs)                    = x : ooToC cs
+toCube :: Int -> Cube
+toCube 0 = Orange
+toCube _ = Brown
+
+toInt :: Stack -> Int
+toInt w = sum $ zipWith (\f c -> f * toDigit c) (1 : 2 : 4 : Nil) w
+
+-- | Get the (first three bits of the) binary representation of a number
+digits :: Int -> Array Int
+digits n = map bit [1, 2, 4]
+    where bit m = if n .&. m == m then 1 else 0
+
+toAStack :: Int -> AStack
+toAStack num = map toCube (digits num)
+
+toStack :: Int -> Stack
+toStack = fromFoldable <<< toAStack
+
+mapNumbers :: (Int -> Int) -> Transformer
+mapNumbers f = map (toInt >>> f >>> toStack)
 
 chapter4 :: Chapter
 chapter4 = {
     name: "Chapter 4",
 
     transformers: fromArray [
-        "mapXtoOX" :> {
-            name: "map {X}↦[{X}{Orange}]",
-            function: map (concatMap (\x -> (Orange : x : Nil)))
+        "mapAdd1" :> {
+            name: "map (+1)",
+            function: mapNumbers (_ + 1)
         },
-        "mapCXtoX" :> {
-            name: "map [{X}{Cyan}]↦{X}",
-            function: map cxToX
+        "mapSub1" :> {
+            name: "map (-1)",
+            function: mapNumbers (_ - 1)
         },
-        "mapOOtoC" :> {
-            name: "map [{Orange}{Orange}]↦{Cyan}",
-            function: map ooToC
+        "mapMul2" :> {
+            name: "map (×2)",
+            function: mapNumbers (_ * 2)
         },
-        "mapCtoO" :> {
-            name: "map {Cyan}↦{Orange}",
-            function: replaceSingle Cyan Orange
+        "mapPow2" :> {
+            name: "map (^2)",
+            function: mapNumbers (\x -> x * x)
+        },
+        "filterEven" :> {
+            name: "filter even",
+            function: filter (toInt >>> even)
         }
     ],
 
     levels: fromArray [
         "4.1" :-> {
-            name: "Brick",
-            help: Just """This chapter introduces wildcard cubes: {X}.""",
-            difficulty: Easy,
-            initial: [[Cyan, Orange], [Cyan, Cyan, Orange], [Orange, Orange], [Cyan, Cyan, Orange], [Cyan, Orange]],
-            target: [[Cyan], [Cyan, Orange], [Cyan], [Cyan, Orange], [Cyan]]
+            name: "0b0 .. 0b111",
+            help: Just """What could be the meaning of the title <code>0b0 .. 0b111</code>?
+                          Read from top to bottom. Calculate modulo eight.""",
+            difficulty: Medium,
+            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
+            target: map toAStack [1, 3, 5, 7, 1, 3, 5, 7]
         },
         "4.2" :-> {
-            name: "Fort",
+            name: "Odd..",
             help: Nothing,
-            difficulty: Hard,
-            initial: [[Cyan, Orange], [Cyan, Cyan, Orange], [Orange, Orange], [Cyan, Cyan, Orange], [Cyan, Orange]],
-            target: [[Orange, Cyan], [Orange, Orange], [Orange, Cyan], [Orange, Orange], [Orange, Cyan]]
+            difficulty: Easy,
+            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
+            target: map toAStack [1, 3, 5, 7]
         },
         "4.3" :-> {
-            name: "Castle",
-            help: Just """This is the last level ... for now. But you can design your own puzzles!
-                          See the <a href="https://github.com/sharkdp/cube-composer">Github repository</a>
-                                           for more information. I hope you enjoyed the game.""",
-            difficulty: Medium,
-            initial: [[Orange], [Orange, Orange], [Orange, Orange, Orange], [Orange, Orange, Orange, Orange], [Orange, Orange, Orange], [Orange, Orange], [Orange]],
-            target: [[Orange, Orange], [Orange, Cyan], [Orange, Orange], [Orange, Cyan], [Orange, Orange], [Orange, Cyan], [Orange, Orange]]
+            name: "Zero",
+            help: Nothing,
+            difficulty: Hard,
+            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
+            target: map toAStack [0, 0, 0, 0, 0, 0, 0, 0]
+        },
+        "4.4" :-> {
+            name: "Don't panic",
+            help: Just """Life, the Universe and Everything...""",
+            difficulty: Hard,
+            initial: map toAStack [0, 1, 2, 3, 4, 5, 6, 7],
+            target: map toAStack [4, 2, 4, 2, 4, 2, 4, 2]
         }
     ]
 }
