1d192a4e95cfeae454cc08c284560e739c1a457e
sharkdp <davidpeter@web.de>
2016-08-07 16:18:56 +0200

    Update Solver

--- cli/Solver.purs
+++ cli/Solver.purs
@@ -5,13 +5,13 @@ module Solver (
 
 import Prelude
 import Data.Function (on)
-import Data.List (List(..), toList, filter, snoc, concatMap, length, sortBy,
+import Data.List (List(..), fromFoldable, filter, snoc, concatMap, length, sortBy,
                   mapMaybe, takeWhile, (:))
 import Data.StrMap as SM
 
-import Levels
-import Transformer
-import Types
+import Levels (getChapter, getLevel, getTransformer)
+import Transformer (transformed)
+import Types (LevelId, TransformerId, Wall, Chapter)
 
 type Solution = List TransformerId
 
@@ -24,14 +24,14 @@ takeShortest (Cons x xs) = x : takeWhile (\y -> length y == length x) xs
 solve' :: Chapter -> Wall -> Wall -> List TransformerId -> List TransformerId -> List Solution
 solve' ch initial target chain ts =
         if final == target
-        then return chain
+        then pure chain
         else takeShortest $ sortBy (compare `on` length)
-                          $ concatMap (\t -> solve' ch initial target (chain `snoc` t) (filter (/= t) ts)) ts
+                          $ concatMap (\t -> solve' ch initial target (chain `snoc` t) (filter (_ /= t) ts)) ts
     where final = transformed (mapMaybe (getTransformer ch) chain) initial
 
 -- | Brute force solver. Returns a list of all shortest solutions, if any exist
 solve :: LevelId -> List Solution
-solve lid = solve' chapter level.initial level.target Nil (toList $ SM.keys chapter.transformers)
+solve lid = solve' chapter level.initial level.target Nil (fromFoldable $ SM.keys chapter.transformers)
     where level = getLevel lid
           chapter = getChapter lid
 
--- cli/cli.purs
+++ cli/cli.purs
@@ -3,12 +3,13 @@ module Main (main) where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.List (List, fromList)
+import Data.Array as A
+import Data.List (List)
 import Data.Traversable (for)
 
-import Types
+import Types (Cube(..))
 import Solver (solve)
-import Levels
+import Levels (levelTitle, getLevel, getChapter, allLevelIds)
 
 ttyColor :: Cube -> Int
 ttyColor Yellow = 0
@@ -18,23 +19,22 @@ ttyColor Red = 3
 ttyColor Cyan = 4
 
 showList :: forall a. (Show a) => List a -> String
-showList xs = show (fromList xs :: Array a)
+showList xs = show (A.fromFoldable xs :: Array a)
 
 showList2 :: forall a. (Show a) => List (List a) -> String
-showList2 xss = show ((fromList <<< map fromList) xss :: Array (Array a))
+showList2 xss = show ((A.fromFoldable <<< map A.fromFoldable) xss :: Array (Array a))
 
 main :: forall eff. Eff (console :: CONSOLE | eff) Unit
-main = do
+main = void do
     for allLevelIds $ \lid -> do
         let chapter = getChapter lid
             level = getLevel lid
             solutions = solve lid
 
         log $ levelTitle lid level
-        log $ "  Initial: " ++ showList2 (map (map ttyColor) level.initial)
-        log $ "  Target:  " ++ showList2 (map (map ttyColor) level.target)
+        log $ "  Initial: " <> showList2 (map (map ttyColor) level.initial)
+        log $ "  Target:  " <> showList2 (map (map ttyColor) level.target)
         log $ "  Solutions: "
         for solutions $ \sol ->
-            log $ "    " ++ showList sol
+            log $ "    " <> showList sol
         log ""
-    return unit
